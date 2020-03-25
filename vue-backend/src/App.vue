6823d899e558bf221771901fee3a022df7a13245
zhengshijie <zhengshijie@78dk.com>
2020-03-25 16:06:53 +0800

    vue-backend

---
+++ vue-backend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+        <transition name="el-zoom-in-center">
+          <router-view></router-view>
+        </transition>
+    </div>
+</template>
+<style>
+    @import "./assets/css/main.css";
+    @import "./assets/css/color-dark.css";     /*深色主题*/
+    /*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
+</style>
